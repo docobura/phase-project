fa28023d23670403aca5aae8e0bcddd8360d7c90
Doc Obura <docdaystar@gmail.com>
2024-05-08 07:28:31 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BotCollection from '../src/components/BotCollection';
+import YourBotArmy from '../src/components/YourBotArmy';
+import BotSpecs from '../src/components/BotSpecs';
+import "../src/App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<Router>
+<div>
+<Routes>
+<Route path="/" element={<BotCollection />} />
+<Route path="/your-bot-army" element={<YourBotArmy />} />
+<Route path="/bots/:botId" element={<BotSpecs />} />
+</Routes>
+</div>
+</Router>
+);
 }
 
 export default App;
---
+++ src/components/SortBar.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+function SortBar({ onSort }) {
+const handleSort = (criteria) => {
+onSort(criteria);
+};
+
+return (
+<div>
+<button onClick={() => handleSort('health')}>Sort by Health</button>
+<button onClick={() => handleSort('damage')}>Sort by Damage</button>
+<button onClick={() => handleSort('armor')}>Sort by Armor</button>
+</div>
+);
+}
+
+export default SortBar;
